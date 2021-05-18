08452fea3cbf8754d8d88d7c24c2ca02797e1b30
James Perrin <jamesperrin88@gmail.com>
2021-05-18 14:37:01 +1000

    Add new Job to jobs jobs index and controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,13 +8,29 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
-  def new; end
+  def new
+    @new_job = Job.new
+  end
 
-  def create; end
+  def create
+    @new_job = Job.new(job_params)
+    @new_job.user_id = current_user.id
+    if @new_job.save
+      redirect_to jobs_path
+    else
+      render action: "new"
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:position, :place, :contract, :description)
+  end
 end
